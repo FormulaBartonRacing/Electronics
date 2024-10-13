8ff727bb81372f85af430581706e8bd8d4a7ee5b
Evans Cesil Thomas <evanscesil@gmail.com>
2024-10-13 19:13:21 +0530

    v2.1

--- Precharge/Docs/resistor.py
+++ Precharge/Docs/resistor.py
@@ -2,26 +2,25 @@ import matplotlib.pyplot as plt
 import math
 import numpy
 vmax = 117.6
-v2 = 90
 c = 4.418	#in mF
-resistor = [100, 120, 150, 180, 210, 270, 330, 390]
+resistor = [100, 120, 150, 180, 220, 270, 330, 390]
 peak_power = []
 time = []
 time_ov = []
 ovload_x = []
 ovload_ovhead = []
 ovload_max = []
+rms = []
 
 R_data =[]
 
-R_ovload_model = [[25, 25, 12.5, 10, 7.5, 6.5, 5, 2.5], [0.1, 1, 2, 2.75, 3.5, 4, 5, 10]]  
-				 # [multiplier], [time in s]
+R_ovload_model = [[25, 25, 12.5, 10, 7.5, 6.5, 5, 2.5], [0.1, 1, 2, 2.75, 3.5, 4, 5, 10]]  # [multiplier], [time in s]
 
-def find_resistor(vmax, C, R_power, percent=97.4):
+def find_resistor(vmax, C, R_power, percent=96):
 	C = C/1000
 	percent /= 100
-	# to account for diode
-	percent = (percent * v2)/(v2 - 0.7)
+	# to account for diode, worst case
+	percent = (percent * vmax)/(vmax - 1)
 	for i in range(len(resistor)):
 		R = resistor[i]
 		t = -R*C*math.log(1-percent)
@@ -35,24 +34,29 @@ def find_resistor(vmax, C, R_power, percent=97.4):
 		ov = round(power/R_power,2)
 		ovload_x.append(ov)
 		R_data.append([R, power, round(t,2), t_no_ov, safety_timer, ov])
+		vt2 = []
 		
-	
+		for tt in numpy.linspace(0,t,2000):
+			vt2.append((vmax*(pow(math.e,-tt/(R*C))))**2)
+		rms.append(round((math.sqrt(numpy.mean(vt2))**2)/R,2))
+			
 		for j in range(len(R_ovload_model[1])-1):
 			if time[i] > R_ovload_model[1][j] and time[i] < R_ovload_model[1][j+1]:
 				x1, x2 = R_ovload_model[1][j], R_ovload_model[1][j+1]
 				y1, y2 = R_ovload_model[0][j], R_ovload_model[0][j+1]
 				y = (time[i]-x1)*(y1-y2)/(x1-x2)+y1
-				ovhead = round(y-ovload_x[i], 2)
+				ovhead = y-ovload_x[i]
 				ovload_max.append(y)
 				ovload_ovhead.append(ovhead)
 				R_data[i].append(ovhead)
 	
-		print(R, "  P_peak:", power, "\tT:",round(t,2), "  OvLoad:", t_no_ov, "  Safety_Timer:", safety_timer)
-		print("      Overload_x:", ov, "\tOverload_max:", round(ovload_max[i]*R_power, 2), "\tOvHead:", ovhead*R_power)
+		print(R, "  P_peak (W):", round(power,2), "\tT (s):",round(t,2), "\t\tOvLoad (s):", t_no_ov)
+		print("      Ovload_x:", ov, "\t\tOvload_max_x:", round(ovload_max[i], 2), "\tOvHead (W):", round(ovhead*R_power,2))
+		print("      P_RMS (W):", rms[i], "\t\tSafety_Timer (s):",round(safety_timer, 2))
 		print()
 		
-find_resistor(vmax, c, 12.5)	
-					
+find_resistor(vmax, c, 12.5)
+	
 #plt.plot(resistor, peak_power)
 #plt.xticks(resistor)
 #plt.yticks(peak_power)
